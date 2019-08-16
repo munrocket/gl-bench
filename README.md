0dd6ae40938dba79b17b867893ec2e122961c5bb
munrocket <munrocket@protonmail.com>
2019-08-16 17:54:35 +0400

    readme

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
-# gl-bench
+# ⏱ gl-bench
 
 WebGL performance benchmark with GPU timers from `EXT_disjoint_timer_query` extension.
-It has fallback to CPU timers but they are not synchronized with the graphics rendering pipeline.
+
+### Motivation
+CPU timers are not synchronized with the graphics rendering pipeline and not guarantee the completion of a potentially
+large amount of graphics work accumulated before the timer is read, and will thus produce wildly inaccurate results.
+glFinish() or readPixels() can be used to determine when previous rendering commands have been completed,
+but will idle the graphics pipeline and adversely affect application performance.
 
 ### Usage
 ```
-// init without passing canvas
-let bench = new GlBench(fps => updateUI(fps));
+/* define logger */
+function logger(fps) {
+  updateUI(fps); //console.log(fps);
+}
+
+/* init benchmark */
+let bench = new GlBench(logger);
 
 function step(timestamp) {
-  // update timer
+  /* update timer */
   bech.update();
 
   // ...some monitored code
@@ -17,12 +27,16 @@ function step(timestamp) {
   requestAnimationFrame(step);
 }
 requestAnimationFrame(step);
-
 ```
 
+### Examples
+- [webgl1 fps](https://munrocket.github.io/gl-bench/examples/fps-webgl1.html)
+- [webgl2 fps](https://munrocket.github.io/gl-bench/examples/fps-webgl1.html)
+
 ### 2do list
--[x] update()
--[x] examples
--[x] webgl2 support
--[ ] begin()+end()
--[ ] own ui
+- [x] update()
+- [x] examples
+- [x] webgl2 support
+- [ ] begin/end into query
+- [ ] deal with GLuint64EXT
+- [ ] own ui
--- src/gl-bench.js
+++ src/gl-bench.js
@@ -1,38 +1,37 @@
 export default class GlBench {
 
-  constructor(uiUpdate, canvas = null) {
+  constructor(uiUpdate) {
     if (typeof uiUpdate == 'function') {
       this.uiUpdate = uiUpdate;
     } else {
       console.error('Wrong fist parameter in new GlBench()')
     }
-    if (canvas && typeof canvas.getContext == 'function') {
-      this.canvas = canvas;
-    }
   }
 
-  init() {
-    if (!this.canvas) {
-      let cs = document.getElementsByTagName('canvas');
-      for (let i = 0; i < cs.length; i++) {
-        if (i == 0 || this.canvas.width * this.canvas.height < cs[i].width * cs[i].height) {
-          this.canvas = cs[i];
+  initCanvas(canvas) {
+    if (canvas && typeof canvas.getContext == 'function') {
+      if (!canvas) {
+        let cs = document.getElementsByTagName('canvas');
+        for (let i = 0; i < cs.length; i++) {
+          if (i == 0 || canvas.width * canvas.height < cs[i].width * cs[i].height) {
+            canvas = cs[i];
+          }
         }
       }
-    }
-    this.gl = this.canvas.getContext('webgl') || this.canvas.getContext('experimental-webgl');
-    if (this.gl) {
-      this.ext = this.gl.getExtension('EXT_disjoint_timer_query');
-      this.gl.createQuery = this.ext.createQueryEXT.bind(this.ext);
-      this.gl.deleteQuery = this.ext.deleteQueryEXT.bind(this.ext);
-      this.gl.beginQuery = this.ext.beginQueryEXT.bind(this.ext);
-      this.gl.endQuery = this.ext.endQueryEXT.bind(this.ext);
-      this.gl.getQueryParameter = this.ext.getQueryObjectEXT.bind(this.ext);
-      this.gl.QUERY_RESULT_AVAILABLE = this.ext.QUERY_RESULT_AVAILABLE_EXT;
-      this.gl.QUERY_RESULT = this.ext.QUERY_RESULT_EXT;
-    } else {
-      this.gl = this.canvas.getContext('webgl2');
-      this.ext = (this.gl) ? this.gl.getExtension('EXT_disjoint_timer_query_webgl2') : null;
+      this.gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+      if (this.gl) {
+        this.ext = this.gl.getExtension('EXT_disjoint_timer_query');
+        this.gl.createQuery = this.ext.createQueryEXT.bind(this.ext);
+        this.gl.deleteQuery = this.ext.deleteQueryEXT.bind(this.ext);
+        this.gl.beginQuery = this.ext.beginQueryEXT.bind(this.ext);
+        this.gl.endQuery = this.ext.endQueryEXT.bind(this.ext);
+        this.gl.getQueryParameter = this.ext.getQueryObjectEXT.bind(this.ext);
+        this.gl.QUERY_RESULT_AVAILABLE = this.ext.QUERY_RESULT_AVAILABLE_EXT;
+        this.gl.QUERY_RESULT = this.ext.QUERY_RESULT_EXT;
+      } else {
+        this.gl = canvas.getContext('webgl2');
+        this.ext = (this.gl) ? this.gl.getExtension('EXT_disjoint_timer_query_webgl2') : null;
+      }
     }
     this.inited = true;
   }
@@ -43,56 +42,56 @@ export default class GlBench {
     } else if (this.inited) {
       this.updateCPU();
     } else {
-      this.init();
+      this.initCanvas();
       this.update();
     }
   }
 
   updateCPU() {
-    if (this.cpuFrame === undefined) {
-      this.cpuFrame = 0;
+    if (this.cpuFrames === undefined) {
+      this.cpuFrames = 0;
       this.prevTime = (performance || Date).now();
     } else {
-      this.cpuFrame++;
+      this.cpuFrames++;
       let time = (performance || Date).now();
-      let elapsed = (time - this.prevTime) / 1000;
-      if (elapsed > 1) {
-        let fps = this.cpuFrame / elapsed;
-        while (elapsed > 1) {
+      let duration = (time - this.prevTime) / 1000;
+      if (duration > 1) {
+        let fps = this.cpuFrames / duration;
+        while (duration > 1) {
           this.uiUpdate(fps);
-          elapsed--;
+          duration--;
         }
-        this.cpuFrame = 0;
+        this.cpuFrames = 0;
         this.prevTime = time;
       }
     }
   }
 
   updateGPU() {
-    if (this.gpuFrame === undefined) {
-      this.query = [ this.gl.createQuery() ];
-      this.gl.beginQuery(this.ext.TIME_ELAPSED_EXT, this.query[0]);
-      this.gpuFrame = 0;
-      this.elapsed = 0;
+    if (this.queriesFrames === undefined) {
+      this.queries = [ this.gl.createQuery() ];
+      this.gl.beginQuery(this.ext.TIME_ELAPSED_EXT, this.queries[0]);
+      this.queriesFrames = 0;
+      this.queriesDuration = 0;
     } else {
       this.gl.endQuery(this.ext.TIME_ELAPSED_EXT);
-      if (!this.gl.getParameter(this.ext.GPU_DISJOINT_EXT)) {
-        for (var i = this.gpuFrame; i < this.query.length; i++) {;
-          if (this.gl.getQueryParameter(this.query[i], this.gl.QUERY_RESULT_AVAILABLE)) {
-            this.elapsed += this.gl.getQueryParameter(this.query[i], this.gl.QUERY_RESULT) / 1e9;
-            this.gpuFrame++;
-            if (this.elapsed > 1) {
-              let fps = this.gpuFrame / this.elapsed;
-              while (this.elapsed > 1) {
+      if (!this.gl.getParameter(this.ext.GPU_DISJOINT_EXT)) { //check for bugs
+        for (var i = this.queriesFrames; i < this.queries.length; i++) {
+          if (this.gl.getQueryParameter(this.queries[i], this.gl.QUERY_RESULT_AVAILABLE)) {
+            this.queriesDuration += this.gl.getQueryParameter(this.queries[i], this.gl.QUERY_RESULT) / 1e9;
+            this.queriesFrames++;
+            if (this.queriesDuration > 1) {
+              let fps = this.queriesFrames / this.queriesDuration;
+              while (this.queriesDuration > 1) {
                 this.uiUpdate(fps);
-                this.elapsed--;
+                this.queriesDuration--;
               }
-              for (let i = 0; i < this.gpuFrame; i++) {
-                this.gl.deleteQuery(this.query[i]);
+              for (let i = 0; i < this.queriesFrames; i++) {
+                this.gl.deleteQuery(this.queries[i]);
               }
-              this.query.splice(0, this.gpuFrame);
-              this.gpuFrame = 0;
-              this.elapsed = 0;
+              this.queries.splice(0, this.queriesFrames);
+              this.queriesFrames = 0;
+              this.queriesDuration = 0;
             }
           }
         }
@@ -101,5 +100,4 @@ export default class GlBench {
       this.gl.beginQuery(this.ext.TIME_ELAPSED_EXT, this.query[this.query.length-1]);
     }
   }
-
 }
